0ce24f1e322bbe881517f6f075327cd40fab17a8
mouhanadGds <mouhanad.ahmed@gds-direct.com>
2025-01-27 13:53:00 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  mode: 'development', // or 'production' depending on your environment
+  entry: './src/email/sendEmail.js', // Replace with your entry point
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
